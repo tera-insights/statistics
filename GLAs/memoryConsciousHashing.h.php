99cb8b4b6215c798340509f3a22611b7d3c6de3f
Jess Smith <smith.jessk@gmail.com>
2017-08-08 18:09:16 -0400

    Properly separate inputs into those for grouping and those for the inner GLA

--- GLAs/memoryConsciousHashing.h.php
+++ GLAs/memoryConsciousHashing.h.php
@@ -1,24 +1,72 @@
-
 <?
-function Memory_Conscious_Hashing(array $t_args, array $inputs, array $outputs)
+// Copyright 2017 Tera Insights, LLC. All Rights Reserved.
+
+function get_inputs_for_bucket_score_GLA(array $t_args, array $inputs, array $outputs, array $states) {
+    $glaInputAtts = [];
+    $glaOutputAtts = [];
+    $gbyAttNames = $t_args['group'];
+
+    grokit_assert( is_array($gbyAttNames), 'Invalid value given for groups, expected an expression name or list of expression names');
+    foreach( $inputs as $name => $type ) {
+      $is_argument_for_bucket_score_gla = !in_array($name, $gbyAttNames);
+      if($is_argument_for_bucket_score_gla) {
+          $glaInputAtts[$name] = $type;
+      }
+    }
+    foreach( $outputs as $name => $type ) {
+        if( !in_array($name, $gbyAttNames) ) {
+            $glaOutputAtts[$name] = $type;
+        }
+    }
+
+    $innerGLA = $t_args['aggregate'];
+    $innerGLA = $innerGLA->apply($glaInputAtts, $glaOutputAtts, $states);
+    $libraries = $innerGLA->libraries();
+
+    $innerRes = get_first_value( $innerGLA->result_type(), [ 'multi', 'single', 'state' ] );
+
+    if( $innerRes == 'state' ) {
+        // If the result type is state, the only output is a state object
+        // containing the GLA.
+        $outputName = array_keys($glaOutputAtts)[0];
+        $innerOutputs = [ $outputName => lookupType('base::STATE', [ 'type' => $innerGLA ] ) ];
+    }
+    else {
+        $innerOutputs = $innerGLA->output();
+
+        grokit_assert(\count($innerOutputs) == \count($glaOutputAtts),
+            'Expected ' . \count($glaOutputAtts) . ' outputs fromm Inner GLA, got ' .
+            \count($innerOutputs));
+    }
+
+    return $glaInputAtts;
+}
+
+function get_grouping_attributes(array $t_args, array $inputs) {
+  $gbyAttNames = $t_args['group'];
+  $grouping_attributes = [];
+  foreach( $inputs as $name => $type ) {
+    $is_grouping_attribute = in_array($name, $gbyAttNames);
+    if($is_grouping_attribute) {
+      $grouping_attributes[$name] = $type;
+    }
+  }
+  return $grouping_attributes;
+}
+
+function Memory_Conscious_Hashing(array $t_args, array $inputs, array $outputs, array $states)
 {
-    // Randomly generate class name
     $className = generate_name("MemoryConsciousHashing");
-
-    // Set output types
-    $outputs = array_combine(array_keys($outputs), $inputs);
-
-    // Process template arguments
-    $initialThreshold = $t_args['initialThreshold'];
-    $thresholdGrowthFactor = $t_args['thresholdGrowthFactor'];
-    $maxProducedGroups = $t_args['maxProducedGroups'];
-    $dieProbability = $t_args['dieProbability'];
-
+    $outputs = ["hashed_key" => lookupType("BASE::INT")];
+    $glaInputs = get_inputs_for_bucket_score_GLA($t_args, $inputs, $outputs, $states);
+    $groupingInputs = get_grouping_attributes($t_args, $inputs);
+    $minimumBucketScorePercentage = $t_args['minimumBucketScorePercentage'];
+    $maxNumberOfBuckets = $t_args['maxNumberOfBuckets'];
+    $arraySize = $t_args['arraySize'];
     $isDebugMode = get_default($t_args, 'debug', 0) > 0;
     $tuplesPerFragment = 200000;
-
     $sys_headers  = ['math.h', 'armadillo', 'random', 'vector', 'stdexcept',
-      'map', 'cstdlib', 'string', 'iostream'];
+      'map', 'cstdlib', 'string', 'iostream', 'array'];
     $user_headers = [];
     $lib_headers  = [];
     $libraries    = ['armadillo'];
@@ -32,39 +80,36 @@ class <?=$className?>;
 class <?=$className?> {
  public:
   using KeySet = std::tuple<<?=typed($inputs)?>>;
-  using Map = std::map<int, int>;
-
-  // For the fragmented result
-  using Iterator = struct {
-    Map::const_iterator current, end;
+  using ScoreType = uint64_t;
+  using ArrayType = std::array<uint64_t, <?=$arraySize?>>;
+  using HashType = uint64_t;
+  
+  using FragmentedResultIterator = struct {
+    ArrayType::const_iterator current, end;
     std::size_t index;
   };
 
-  const double initialThreshold = <?=$initialThreshold?>;
-  const double thresholdGrowthFactor = <?=$thresholdGrowthFactor?>;
-  const int maxProducedGroups = <?=$maxProducedGroups?>;
-  const double dieProbability = <?=$dieProbability?>;
   static constexpr std::size_t kFragmentSize = <?=$tuplesPerFragment?>;
+  const float minimum_bucket_score_percentage = <?=$minimumBucketScorePercentage?>;
+  const uint64_t max_number_of_buckets = <?=$maxNumberOfBuckets?>;
 
-  // Maps hashed group key to the bucket score. Multiple groups hash to the
-  // same bucket.
-  Map bucket_scores;
+  ArrayType aggregate_scores;
 
-  // Fields used for the fragment result type.
-  std::vector<Map::const_iterator> iterators;
+  std::vector<ArrayType::const_iterator> iterators_for_fragmented_result;
 
-  int get_bucket_key(KeySet group) {
-    
+  HashType get_bucket_key(KeySet group) {
+    return SpookyHash(Hash(group), offset);
   }
 
-  bool groupWillSurvive(KeySet group) {
-    return frequency_map[group] > minimum_group_size;
+  bool bucket_will_survive(HashType key) {
+    float minimum_score = minimum_bucket_score_percentage * total_score;
+    return aggregate_scores[key] > minimum_score;
   }
 
-  int numberOfGroupsThatWillSurvive() {
+  int numberOfBucketsThatWillSurvive() {
     int survivors = 0;
-    for (auto it = frequency_map.begin(); it != frequency_map.end(); it++) {
-      if (groupWillSurvive(it->first)) {
+    for (auto it = aggregate_scores.begin(); it != aggregate_scores.end(); it++) {
+      if (bucket_will_survive(*it)) {
         survivors++;
       }
     }
@@ -72,38 +117,19 @@ class <?=$className?> {
   }
 
   bool isTooMuchMemoryUsed() {
-    return numberOfGroupsThatWillSurvive() > maximum_groups_allowed;
+    return numberOfBucketsThatWillSurvive() > maxNumberOfBuckets;
   }
 
   bool isKeyNew(KeySet key) {
     return frequency_map.find(key) == frequency_map.end();
   }
 
-  bool shouldSampleTuple() {
-    double random = rand() * 1.0 / RAND_MAX;
-    return random < samplingRate;
+  KeySet get_group_key(<?=const_typed_ref_args($groupingInputs)?>) {
+    return KeySet(<?=args($groupingInputs)?>);
   }
 
-  KeySet get_group_key(<?=const_typed_ref_args($inputs)?>) {
-    return KeySet(<?=args($inputs)?>);
-  }
-
-  void resampleMap() {
-    for (auto it = frequency_map.begin(); it != frequency_map.end(); it++) {
-      auto key = it->first;
-      auto originalCount = it->second;
-      int newNumberSampled = 0;
-      for (int trial = 0; trial < originalCount; trial++) {
-        if (shouldSampleTuple()) {
-          newNumberSampled++;
-        }
-      }
-      if (newNumberSampled == 0) {
-        frequency_map.erase(key);
-      } else {
-        frequency_map[key] = newNumberSampled;
-      }
-    }
+  ScoreType get_aggregate_score(<?=const_typed_ref_args($glaInputs)?>) {
+    
   }
 
  public:
@@ -113,19 +139,8 @@ class <?=$className?> {
   }
 
   void AddItem(<?=const_typed_ref_args($inputs)?>) {
-    if (!shouldSampleTuple()) {
-      return;
-    }
-
-    const KeySet key = get_group_key(<?=args($inputs)?>);
-    if (isKeyNew(key)) {
-      frequency_map[key] = 0;
-    }
-    frequency_map[key]++;
-    if (isTooMuchMemoryUsed()) {
-      samplingRate *= reductionRate;
-      resampleMap();
-    }
+    const KeySet key = get_group_key(<?=args($groupingInputs)?>);
+    frequency_map[key] += get_aggregate_score(<?=args($glaInputs)?>);
   }
 
   // Merges `this->frequency_map` and `other->frequency_map`.
