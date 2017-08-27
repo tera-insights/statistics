b4341337af577d89304a2da2e497d7ff16bbaa18
Jess Smith <smith.jessk@gmail.com>
2017-08-27 16:24:44 -0400

    Use constant state for MCH segmentation

--- GLAs/memoryConsciousHashing.h.php
+++ GLAs/memoryConsciousHashing.h.php
@@ -1,5 +1,77 @@
 <?
 // Copyright 2017 Tera Insights, LLC. All Rights Reserved.
+function Memory_Conscious_Hashing_Constant_State(array $t_args) {
+    $className = $t_args['className'];
+    $sys_headers  = ['atomic', 'array', 'unordered_map', 'cstdint'];
+    $user_headers = [];
+    $lib_headers  = [];
+    $libraries    = [];
+    $maximumGroupsProduced = $t_args['maximumGroupsProduced'];
+    $numberOfBuckets = $t_args['numberOfBuckets'];
+    $scoreType = $t_args['scoreType'];
+?>
+
+class <?=$className?>ConstantState {
+ private:
+  using HashType = uint64_t;
+  using AtomicScoreType = std::atomic<<?=$scoreType?>>;
+  using ScoreArray = std::array<<?=$scoreType?>, <?=$numberOfBuckets?>>;
+
+  ScoreArray scores;
+  AtomicScoreType minimum_score;
+
+  AtomicScoreType total_score;
+
+  uint64_t get_bucket_index(HashType hash) const {
+    double ratio = (hash * 1.0) / UINT64_MAX;
+    uint64_t product = scores.size() * ratio;
+    if (product == scores.size()) {
+      return scores.size() - 1;
+    } else {
+      return product;
+    }
+  }
+
+ public:
+    friend class <?=$className?>;
+
+    <?=$className?>ConstantState()
+      : minimum_score(0),
+        total_score(0) {
+    }
+
+    bool IsGroupSurvivor(HashType hash) const {
+      auto bucket_index = get_bucket_index(hash);
+      return scores.at(bucket_index) >= minimum_score;
+    }
+
+    template <typename T>
+    void AddMap(const T &map) {
+      for (auto it = map.begin(); it != map.end(); it++) {
+        auto bucket_index = get_bucket_index(it->first);
+        long score;
+        it->second.GetResult(score);
+        scores[bucket_index] += score;
+        total_score += score;
+      }
+    }
+
+    double GetMinimumScore() const {
+      return minimum_score;
+    }
+};
+
+<?
+    return [
+        'kind'           => 'RESOURCE',
+        'name'           => $className . 'ConstantState',
+        'system_headers' => $sys_headers,
+        'user_headers'   => $user_headers,
+        'lib_headers'    => $lib_headers,
+        'libraries'      => $libraries,
+        'configurable'   => false,
+    ];
+}
 
 function get_inputs_for_bucket_score_GLA(array $t_args, array $inputs) {
     $glaInputs = [];
@@ -41,16 +113,8 @@ function Memory_Conscious_Hashing(array $t_args, array $inputs, array $outputs, 
     $glaInputs = get_inputs_for_bucket_score_GLA($t_args, $inputs);
     $groupingInputs = get_grouping_attributes($t_args, $inputs);
     $innerGLA = get_inner_gla($t_args, $inputs, $states);
-    $minimumTotalScoreMultiplier = $t_args['minimumTotalScoreMultiplier'];
-    $maxNumberOfBucketsProduced = $t_args['maxNumberOfBucketsProduced'];
-    $initialNumberOfBuckets = $t_args['initialNumberOfBuckets'];
-    $numberOfSegments = $t_args['numberOfSegments'];
-    $bucketsPerSegment = ceil($initialNumberOfBuckets / $numberOfSegments);
-    $isDebugMode = get_default($t_args, 'debug', 0) > 0;
-    $tuplesPerFragment = 200000;
     $sys_headers  = ['math.h', 'armadillo', 'random', 'vector', 'stdexcept',
       'map', 'cstdlib', 'string', 'iostream', 'array', 'list', 'iterator', 'limits'];
-    $seed = 0;
     $user_headers = [];
     $lib_headers  = ['base\HashFct.h', 'statistics\MemoryEstimators.h'];
     $libraries = $innerGLA->libraries();
@@ -61,111 +125,61 @@ function Memory_Conscious_Hashing(array $t_args, array $inputs, array $outputs, 
 
 class <?=$className?>;
 
+<?  $state = lookupResource(
+  "statistics::Memory_Conscious_Hashing_Constant_State", [
+    'className' => $className,
+    'minimumTotalScoreMultiplier' => $t_args['minimumTotalScoreMultiplier'],
+    'maximumGroupsProduced' => $t_args['maximumGroupsProduced'],
+    'numberOfBuckets' => $t_args['numberOfBuckets'],
+    'scoreType' => 'uint16_t',
+  ]
+); ?>
+
 class <?=$className?> {
  public:
-  static const uint64_t numberOfSegments = <?=$numberOfSegments?>;
-  static const uint64_t bucketsPerSegment = <?=$bucketsPerSegment?>;
-
-  using KeySet = std::tuple<<?=typed($groupingInputs)?>>;
   using InnerGLA = <?=$innerGLA?>;
-  using ScoreType = unsigned long;
-  using ArrayType = std::array<<?=$innerGLA?>, bucketsPerSegment>;
-  using ArrayOfArraysType = std::list<ArrayType>;
-  using ScoreArrayType = std::array<ScoreType, bucketsPerSegment>;
-  using ArrayOfScoreArrayType = std::array<ScoreArrayType, numberOfSegments>;
   using HashType = uint64_t;
-  using HalfHashType = uint32_t;
+  using AggregateMap = std::unordered_map<HashType, InnerGLA>;
+  using State = <?=$state?>;
 
-  static constexpr std::size_t kFragmentSize = <?=$tuplesPerFragment?>;
-  const float min_total_score_multiplier = <?=$minimumTotalScoreMultiplier?>;
-  const uint64_t max_number_of_buckets_produced = <?=$maxNumberOfBucketsProduced?>;
-  static const constexpr HashType seed = <?=$seed?>;
-  const uint64_t initialNumberOfBuckets = <?=$initialNumberOfBuckets?>;
-  
-  ArrayOfArraysType segments;
-  ArrayOfScoreArrayType scores;
-  ScoreType total_score;
+  static const constexpr HashType seed = 0;
+  State& state;
+  AggregateMap chunk_aggregate_map;
 
-  std::vector<FragmentedResultIterator<HashType>> result_iterators;
-
-  <?=$className?>() {
-    for (size_t i = 0; i < numberOfSegments; i++) {
-      segments.push_back(ArrayType());
-    }
+  <?=$className?>(const State& const_state)
+    :state (*(State *)(&const_state)) {
   }
 
-  void calculate_segmented_scores_and_total_score() {
-    auto buckets_seen = 0;
-    auto segment_it = segments.begin();
-    auto segments_seen = 0;
-    while (segment_it != segments.end()) {
-      for (size_t i = 0; i < segment_it->size() && buckets_seen < initialNumberOfBuckets; i++) {
-        long score;
-        segment_it->at(i).GetResult(score);
-        total_score += score;
-        scores[segments_seen][i] += score;
-        buckets_seen++;
-      }
-      segment_it = segments.erase(segment_it);
-      segments_seen++;
-    }
-  }
-
-  int get_segment_index(HashType hash) const {
-    HashType upper_half_of_hash = (hash >> 32) & 0xFFFFFFFFLL;
-    double ratio = (upper_half_of_hash * 1.0) / (1UL << 32);
-    return ratio * numberOfSegments;
-  }
-
-  int get_bucket_index(HashType hash) const {
-    HashType lower_half_of_hash = hash & 0xFFFFFFFFLL;
-    double ratio = (lower_half_of_hash * 1.0) / (1UL << 32);
-    return ratio * bucketsPerSegment;
+  bool isHashNew(HashType hash) {
+    return chunk_aggregate_map.find(hash) == chunk_aggregate_map.end();
   }
 
  public:
-  ArrayOfArraysType::iterator get_segment(int index) {
-    auto it = segments.begin();
-    std::advance(it, index);
-    return it;
-  }
-
   void AddItem(<?=const_typed_ref_args($inputs)?>) {
     auto hash = chainedHash(<?=args($groupingInputs)?>);
-    auto segment = get_segment(get_segment_index(hash));
-    segment->at(get_bucket_index(hash)).AddItem(<?=args($glaInputs)?>);
+    if (isHashNew(hash)) {
+      chunk_aggregate_map[hash] = InnerGLA();
+    }
+    chunk_aggregate_map[hash].AddItem(<?=args($glaInputs)?>);
   }
 
   void AddState(<?=$className?>& other) {
-    ArrayOfArraysType other_map = other.GetAggregateScores();
-    for (size_t i = 0; i < other_map.size(); i++) {
-      auto other_segment = other.get_segment(i);
-      for (size_t j = 0; j < other_segment->size(); j++) {
-        auto my_segment = get_segment(i);
-        my_segment->at(j).AddState(other_segment->at(j));
+    auto other_map = other.GetAggregateMap();
+    for (auto it = other_map.begin(); it != other_map.end(); it++) {
+      if (isHashNew(it->first)) {
+        chunk_aggregate_map[it->first] = InnerGLA();
       }
+      chunk_aggregate_map[it->first].AddState(it->second);
     }
   }
 
-  const ArrayOfArraysType &GetAggregateScores() const {
-    return segments;
+  const AggregateMap &GetAggregateMap() const {
+    return chunk_aggregate_map;
   }
 
-  void FinalizeState() {
-    calculate_segmented_scores_and_total_score();
-  }
-
-  FragmentedResultIterator<HashType>* Finalize(int fragment) {
-    return &result_iterators.at(fragment);
-  }
-
-  bool GetNextResult(FragmentedResultIterator<ScoreType>* it, <?=typed_ref_args($outputs)?>) {
-    if (it->current == it->end) {
-      return false;
-    }
-    hashed_key = *(it->current);
-    ++it->current;
-    return true;
+  void ChunkBoundary() {
+    state.AddMap(chunk_aggregate_map);
+    chunk_aggregate_map.clear();
   }
 
   HashType chainedHash(<?=const_typed_ref_args($groupingInputs)?>) const {
@@ -178,12 +192,10 @@ class <?=$className?> {
 
   bool IsGroupSurvivor(<?=const_typed_ref_args($groupingInputs)?>) const {
     auto hash = chainedHash(<?=args($groupingInputs)?>);
-    auto segment_index = get_segment_index(hash);
-    auto bucket_index = get_bucket_index(hash);
-    auto bucket_score = scores.at(segment_index).at(bucket_index);
-    auto minimum_score = min_total_score_multiplier * total_score;
-    return minimum_score <= bucket_score;
+    return state.IsGroupSurvivor(hash);
   }
+
+  void FinalizeState() {}
 };
 
 <?
@@ -200,6 +212,8 @@ class <?=$className?> {
         'output'         => $outputs,
         'result_type'    => ['state'],
         'finalize_as_state' => true,
+        'chunk_boundary' => true,
+        'generated_state' => $state,
     ];
 }
 ?>
